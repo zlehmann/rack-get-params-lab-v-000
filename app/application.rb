b7d7435fbde35682b9b03eba6e7a40ec213e2054
Zachary Lehmann <zlehmann@gmail.com>
2018-11-24 22:32:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -19,7 +19,7 @@ class Application
         resp.write "Your cart is empty"
       else
         @@cart.each do |item|
-          resp.write "#{item}"
+          resp.write "#{item}\n"
         end
       end
     else
